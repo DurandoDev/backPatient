afed3d48b89d716bdfcebc3451f1afbc2632770b
DurandoDev <nat.durando@gmail.com>
2024-03-09 01:19:37 +0100

    fixing auto-increment bis

--- src/main/java/com/medilabosolutions/model/Patient.java
+++ src/main/java/com/medilabosolutions/model/Patient.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class Patient {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer id;
 
 	@NotBlank (message = "Firstname is mandatory")
